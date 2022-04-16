795074dc2764db76fe3edd2cc641653d789757f6
garry332211 <gurvindersinghoo7@hotmail.com>
2022-04-16 18:58:02 +1200

    Assignment 1 Completed

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,8 @@
 
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseData from "./components/ExpenseData";
+
 function App() {
+
   const expense = [
 
     { title: "Car Insurance", amount: 200.23, date: new Date(2022, 4, 15) },
@@ -8,33 +10,10 @@ function App() {
     { title: "House Rent", amount: 2000, date: new Date(2022, 3, 1) },
     { title: "Mortgage", amount: 550.22, date: new Date(2022, 4, 30) },
   ];
-
   return (
     <div>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-
-<ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-
-<ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
-      
+     
+      <ExpenseData items={expense} />
     
     </div>
 
--- react-complete-guide/src/components/ExpenseDate.js
+++ react-complete-guide/src/components/ExpenseDate.js
@@ -1,6 +1,6 @@
 
 
-import React from 'react'
+
 
 function ExpenseDate(props) {
     const month = props.date.toLocaleString("en-US", { month: "long" });
