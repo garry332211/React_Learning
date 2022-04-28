b68a5b5111d33ca5a387d97ac81d2d134ad59960
garry332211 <gurvindersinghoo7@hotmail.com>
2022-04-28 22:00:40 +1200

    Form Tak Agya Samjh Hun Agge

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Form from './components/From';
+import Tables from './components/Tables';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <h1>
+          Hello How Are You My friend
+        </h1>
+        <hr/>
+        <h3>Blow is the list of people who failed the exam</h3>
+
+        <Tables/>
+        <hr/><hr/><hr/>
+         
+         <Form/>
     </div>
   );
 }
