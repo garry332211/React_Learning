4ac1e112d946c28c8bfae1131372352c6ed505d7
garry332211 <gurvindersinghoo7@hotmail.com>
2022-06-18 21:59:43 +1200

    margin sorted

--- ReactWithMax/Practice Project/foord-ordering-system/src/components/MainPage.js
+++ ReactWithMax/Practice Project/foord-ordering-system/src/components/MainPage.js
@@ -88,13 +88,11 @@ const MainPage = () => {
                 </em>
               </strong>
             </h2>
-            <p>The ingredients that are being used are traditional herbs and spices which will make you sniff the opulent aroma of Indian subcontinent. At the same time our cordial staffs will be there to ensure that you are having a serene time with your peers or family members.</p>
+          
 
-<p><strong><em><u>Our dishes are completely vegetarian</u></em></strong>– You would be proud to know that all our dishes are vegetarian, without a trace of animal content of any sort. So you can be rest assured of having some of the healthy foods, which would be absolutely superlative for your appetite. All our foods are being available for dining and some of them can be takeaway-ed.</p>
 
-<p><strong><em><u>Are you in need of exotic desserts</u></em></strong><strong><em><u>? Do drop in here</u></em></strong>– Apart from the mouth watering dishes, it is a privilege to announce that we do serve multitude of confectionary and snacks. You will also get array of exotic desserts for the special occasions. You can check out our range in the menu and sweets section.</p>
 
-<p>We are being located on Mt. Roskill and Papatoetoe and are available for functions and private venue hire. Book us today so that we could build up a positive bond of amity with all our clients.</p>
+
           </div>
           <AvailableMeals />
           
